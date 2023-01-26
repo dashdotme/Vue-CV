7a91ae760d082cf8040905a9f834ced48519cfe3
Dash Vallance <35617767+dashdotme@users.noreply.github.com>
2023-01-26 16:47:33 +1300

    fix: vue pipeline configs

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: '/Vue-CV/' // for github pages deployment
+   base: "/Vue-CV/" // for github pages deployment
+   // "./" for local deployment
 });
